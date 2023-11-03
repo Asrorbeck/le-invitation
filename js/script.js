c31f8fc4f1d68d45f7292d040a6066e60230cfd0
Asrorbeck <2010288.asrorbek.tursunpulatov@gmail.com>
2023-11-03 11:08:40 +0500

    btn-music

--- js/script.js
+++ js/script.js
@@ -1,32 +1,86 @@
+const musicControl = document.getElementById("navbar__btn");
+const audio = document.getElementById("background-music");
+const musicIcon = document.getElementById("music-icon");
+
+let isPlaying = true;
+
+var modal = document.getElementById("myModal");
+var yesBtn = document.getElementById("yesBtn");
+var noBtn = document.getElementById("noBtn");
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+yesBtn.addEventListener("click", function () {
+  audio.muted = false
+  audio.play();
+  musicIcon.src = "./img/unmute.svg";
+  closeModal();
+
+  isPlaying = !isPlaying;
+});
+
+noBtn.addEventListener("click", function () {
+  audio.pause();
+  musicIcon.src = "./img/mute.svg";
+  closeModal();
+});
+
+window.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
+
+window.onload = function () {
+  modal.style.display = "block";
+}
+
+musicControl.addEventListener("click", function () {
+  if (isPlaying) {
+    console.log('ok');
+    audio.muted = false
+    audio.play();
+    musicIcon.src = "./img/unmute.svg";
+  } else {
+    audio.pause();
+    musicIcon.src = "./img/mute.svg";
+  }
+
+  isPlaying = !isPlaying;
+});
+
+
 const targetDate = new Date("2023-12-31T00:00:00").getTime();
 
-    const countdown = setInterval(function () {
-      const now = new Date().getTime();
-      const timeRemaining = targetDate - now;
+const countdown = setInterval(function () {
+  const now = new Date().getTime();
+  const timeRemaining = targetDate - now;
 
-      if (timeRemaining <= 0) {
-        clearInterval(countdown);
-        console.log("Bugun to'y");
-      } else {
-        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+  if (timeRemaining <= 0) {
+    clearInterval(countdown);
+    console.log("Bugun to'y");
+  } else {
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-        document.getElementById("days").innerText = days;
-        document.getElementById("hours").innerText = hours;
-        document.getElementById("minutes").innerText = minutes;
-        document.getElementById("seconds").innerText = seconds;
-      }
-    }, 1000);
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+  }
+}, 1000);
 
-    function handleSubmit(event) {
-      event.preventDefault();
-      const formData = new FormData(event.target); 
-      console.log("Form Data:", Object.fromEntries(formData)); 
-      event.target.reset();
-      alert("Sizning xabaringiz muvvaffaqiyatli yetkazildi!")
-    }
+function handleSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  console.log("Form Data:", Object.fromEntries(formData));
+  event.target.reset();
+  alert("Sizning xabaringiz muvvaffaqiyatli yetkazildi!")
+}
 
 // ////////////////////////////////////////////////////
 
@@ -45,24 +99,5 @@ document.getElementById("openMaps").addEventListener("click", function () {
 
 // ////////////////////////////////////////////////////////
 
-const musicControl = document.getElementById("navbar__btn");
-const audio = document.getElementById("background-music");
-const musicIcon = document.getElementById("music-icon");
-
-let isPlaying = true;
-
-musicControl.addEventListener("click", function () {
-  if (isPlaying) {
-    audio.play();
-    // musicControl.textContent = "Turn On Music";
-    musicIcon.src = "./img/unmute.svg"; // Update to the mute icon
-  } else {
-    audio.pause();
-    // musicControl.textContent = "Turn Off Music";
-    musicIcon.src = "./img/mute.svg"; // Update to the unmute icon
-  }
-
-  isPlaying = !isPlaying;
-});
 
 
